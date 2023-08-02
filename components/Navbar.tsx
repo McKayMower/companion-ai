a6ce3d1912518edc2467906580f59932d3abd410
McKay Mower <mmower777@gmail.com>
2023-08-01 20:52:50 -0600

    Sidebar and mobile sidebar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,12 +1,13 @@
 "use client"
 
+import MobileSidebar from "@/components/MobileSidebar";
+import { ModeToggle } from "@/components/ModeToggle";
+import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
 import { UserButton } from "@clerk/nextjs";
-import { Menu, Sparkles } from "lucide-react";
+import { Sparkles } from "lucide-react";
 import { Poppins } from "next/font/google";
 import Link from "next/link";
-import { Button } from "@/components/ui/button";
-import { ModeToggle } from "./ModeToggle";
 
 const font = Poppins({
     weight: "600",
@@ -15,9 +16,9 @@ const font = Poppins({
 
 const Navbar = () => {
     return (
-        <div className="fixed w-full z-50 flex justify-between items-center py-2 px-4 border-b border-primary/10 bg-secondary">
+        <div className="fixed w-full z-50 flex justify-between items-center py-2 px-4 border-b border-primary/10 bg-secondary h-16">
             <div className="flex items-center">
-                <Menu className="block md:hidden" />
+                <MobileSidebar />
                 <Link href="/">
                     <h1 className={cn("hidden md:block text-xl md:text-3xl font-bold text-primary", font.className)}>
                         companion.ai
