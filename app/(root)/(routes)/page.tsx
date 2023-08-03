81be91d45a64702463c61675ed0cfa9d3be21bf7
McKay Mower <mmower777@gmail.com>
2023-08-02 21:13:59 -0600

    category filters

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,11 +1,16 @@
+import Categories from "@/components/Categories";
 import SearchInput from "@/components/SearchInput";
+import prismadb from "@/lib/prismadb";
 
-const RootPage = () => {
-    return (
+const RootPage = async () => {
+  const categories = await prismadb.category.findMany();
+
+  return (
     <div className="h-full p-4 space-y-2">
-        <SearchInput />
+      <SearchInput />
+      <Categories data={categories} />
     </div>
-    );
-}
+  );
+};
 
 export default RootPage;
