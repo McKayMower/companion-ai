578c12d85124e215741a8c430454e259fb2fb45c
McKay Mower <mmower777@gmail.com>
2023-08-06 17:41:35 -0600

    stripe webhook

--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,9 @@ import { authMiddleware } from "@clerk/nextjs";
 // This example protects all routes including api/trpc routes
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/nextjs/middleware for more information about configuring your middleware
-export default authMiddleware({});
+export default authMiddleware({
+  publicRoutes: ['/api/webhook'] // dont protect this route, since stripe is not using a logged in user
+});
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
