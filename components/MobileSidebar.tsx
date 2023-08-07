91b5cdcc836697e933ef54d35618ad5591d1db03
McKay Mower <mmower777@gmail.com>
2023-08-06 22:14:51 -0600

    mobile sidebar fix

--- components/MobileSidebar.tsx
+++ components/MobileSidebar.tsx
@@ -1,25 +1,23 @@
 import { Menu } from "lucide-react";
 
-import {
-    Sheet, 
-    SheetContent,
-    SheetTrigger
-} from "@/components/ui/sheet"
-import Sidebar from "@/components/Sidebar"
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import Sidebar from "@/components/Sidebar";
 
-
-const MobileSidebar = ({}) => {
-    return ( 
-        
-        <Sheet>
-            <SheetTrigger className="md:hidden pr-4">
-                <Menu />
-            </SheetTrigger>
-            <SheetContent side={"left"} className="p-0 bg-secondary pt-10 w-32">
-                <Sidebar />
-            </SheetContent>
-        </Sheet>
-     );
+interface MobileSidebarProps {
+  isPro: boolean;
 }
- 
+
+const MobileSidebar = ({ isPro }: MobileSidebarProps) => {
+  return (
+    <Sheet>
+      <SheetTrigger className="md:hidden pr-4">
+        <Menu />
+      </SheetTrigger>
+      <SheetContent side={"left"} className="p-0 bg-secondary pt-10 w-32">
+        <Sidebar isPro={isPro} />
+      </SheetContent>
+    </Sheet>
+  );
+};
+
 export default MobileSidebar;
